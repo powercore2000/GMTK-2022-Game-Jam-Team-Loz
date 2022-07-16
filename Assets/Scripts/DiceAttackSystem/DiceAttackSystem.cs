18c313e911538632c03a212392ebe48717c6b880
powercore2000 <bcory00@yahoo.com>
2022-07-16 09:05:30 -0700

    Polished Dice Throwing Scripts

--- Assets/Scripts/DiceAttackSystem/DiceAttackSystem.cs
+++ Assets/Scripts/DiceAttackSystem/DiceAttackSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DiceAttack
+namespace DiceThrowing
 {
     public class DiceAttackSystem : MonoBehaviour
     {
--- Assets/Scripts/DiceAttackSystem/ProjectileDice.cs
+++ Assets/Scripts/DiceAttackSystem/ProjectileDice.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace DiceAttack
+
+
+namespace DiceThrowing
 {
     public class ProjectileDice : MonoBehaviour
     {
