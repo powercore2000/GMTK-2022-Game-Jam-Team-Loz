723c0524ff141ce9324cf899248b9adcef4be1f5
LegendOfZia <94755908+LegendOfZia@users.noreply.github.com>
2022-07-16 22:45:06 -0400

    Progressing On Menu Systems and Scene Loading
    Issues with the scene manager, but I have made progress in creating a don't destroy on load object to hold the scene manager scripts, that can be easily found and accessed by other game objects. It can also include a listener script for activating the pause menu.

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EnviromentSystems.SceneLoading;
 
 public class MainMenuControl : MonoBehaviour
 {
+    public GameObject sceneMgr;
+    public SceneManagerBehaviour sceneMgrBhv;
+
+    public void Start()
+    {
+        sceneMgr = GameObject.Find("SceneManager");
+        sceneMgrBhv = sceneMgr.GetComponent<SceneManagerBehaviour>();
+    }
+
     public void StartNewGame()
     {
+        sceneMgrBhv.LoadNewLevelByIndex(1);
         //if save data exists
-            //display pop-up window "This will delete existing save data. Continue?"
-                //if yes
-                    //clear player save data
-                    //scene transfer to first level
-                //else
-                    //close dialog box
+        //display pop-up window "This will delete existing save data. Continue?"
+        //if yes
+        //clear player save data
+        //scene transfer to first level
         //else
-            //scene transfer to first level
+        //close dialog box
+        //else
+        //scene transfer to first level
 
     }
 
